6a04c04c4e27cd89b578b61f37b211ea26a13a5f
Leo Gervilla <leo.gervilla@getweave.com@Leos-MacBook-Pro-242.local>
2020-08-22 14:57:50 -0600

    adding to sql file to connect to my db

---
+++ bamazon.sql
@@ -0,0 +1,8 @@
+-- Drops the bamazon_db if it exists currently --
+DROP DATABASE IF EXISTS bamazon_db;
+
+-- Creates the database --
+CREATE DATABASE bamazon_db;
+
+-- Makes so all the following code will affect bamazon_db --
+USE bamazon_db;
