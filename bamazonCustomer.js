de3bcbb3830cfa978f148874f7278cb5a118e07d
Leo Gervilla <leo.gervilla@getweave.com@Leos-MacBook-Pro-242.local>
2020-08-22 14:41:48 -0600

    setting connection string to SQL

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -2,3 +2,11 @@ var inquirer = require("inquirer");
 var mySQL = require("mysql");
 var Table = require("cli-table3");
 const chalk = require("chalk");
+
+var connection = mySQL.createConnection({
+    host: "127.0.0.1",
+    port: 3306,
+    user: "root",
+    password: "password",
+    database: "bamazon_db"
+});
