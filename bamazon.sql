d0561ac691ad1ba8fc30d8057625113eb1e006b1
Leo Gervilla <leo.gervilla@getweave.com@Leos-MacBook-Pro-242.local>
2020-08-22 14:59:37 -0600

    inputtec the code for the creation of the products table within my db

--- bamazon.sql
+++ bamazon.sql
@@ -6,3 +6,13 @@ CREATE DATABASE bamazon_db;
 
 -- Makes so all the following code will affect bamazon_db --
 USE bamazon_db;
+
+-- Creates the "products" table within bamazon_db --
+CREATE TABLE products
+(
+    item_id INTEGER(10) NOT NULL,
+    product_name VARCHAR(50) NOT NULL,
+    department_name VARCHAR(50) NOT NULL,
+    price DECIMAL(10,2) NOT NULL,
+    stock_quantity INTEGER(10) NOT NULL
+);
